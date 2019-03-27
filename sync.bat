abfc2400ffa47846e7b4b4e9dd235afec77c11ff
WangZhen <wangzhenjjcn@gmail.com>
2019-03-28 07:24:27 +0800

    Update sync.bat

--- sync.bat
+++ sync.bat
@@ -38,6 +38,8 @@ echo %gitversion%|find /i "version" >nul && echo %gitversion% || goto gitInstall
 git config --global credential.helper store
 REM git credential-manager uninstall
 Echo %date:~0,4%-%date:~5,2%-%date:~8,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%    finish check git>>"%rootdirx%/log.txt"
+ping -n 2 localhost 1>nul 2>nul
+REM cls
 goto configCheck
 
 
@@ -78,11 +80,14 @@ if not exist %config_file% cd.>>%config_file%
 find ^"github_addr^" %config_file% >>nul &&echo github_addr readed.||echo github_addr not founded && goto githubAddr
 find ^"oschina_addr^" %config_file% >>nul &&echo oschina_addr readed.||echo oschina_addr not founded && goto oschinaAddr
 Echo %date:~0,4%-%date:~5,2%-%date:~8,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%    config ok>>"%rootdirx%/log.txt"
+ping -n 2 localhost 1>nul 2>nul
+REM cls
 goto fileCheck
 
 
 
 :fileCheck
+@title  检查文件系统...
 Echo %date:~0,4%-%date:~5,2%-%date:~8,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%    start check file>>"%rootdirx%/log.txt"
 echo 检查文件系统...
 if exist %tmpdirx% (
@@ -154,17 +159,22 @@ REM Echo %date:~0,4%-%date:~5,2%-%date:~8,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%
 echo %oschina_git_log%|find /i "Already" >nul && echo %oschina_git_log% check sucess || echo  goto resetoschina
 Echo %date:~0,4%-%date:~5,2%-%date:~8,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%    %oschina_git_log% oschina check sucess>>"%rootdirx%/log.txt"
 echo 文件系统检查完毕
+ping -n 2 localhost 1>nul 2>nul
+REM cls
 goto checkSyncDir
 
 
 
 :resetgithub
+@title  resetgithub...
 Echo %date:~0,4%-%date:~5,2%-%date:~8,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%    github reset>>"%rootdirx%/log.txt"
 cd %tmpdirx%
 rd  /S /Q %tmpdirx%\github
 md %tmpdirx%\github
 cd  %tmpdirx%\github
 Echo %date:~0,4%-%date:~5,2%-%date:~8,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%     github reset sucess>>"%rootdirx%/log.txt"
+ping -n 2 localhost 1>nul 2>nul
+REM cls
 goto fileCheck
 
 :resetoschina
@@ -174,10 +184,13 @@ rd  /S /Q %tmpdirx%\oschina
 md %tmpdirx%\oschina
 cd  %tmpdirx%\oschina
 Echo %date:~0,4%-%date:~5,2%-%date:~8,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%     oschina reset sucess>>"%rootdirx%/log.txt"
+ping -n 2 localhost 1>nul 2>nul
+REM cls
 goto fileCheck
 
 
 :gitInstall
+@title  gitInstall...
 cd %rootdirx%
 Echo %date:~0,4%-%date:~5,2%-%date:~8,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%    start check file>>"%rootdirx%/log.txt"
 echo 检查文件系统...
@@ -203,6 +216,7 @@ echo start install git...
 Echo %date:~0,4%-%date:~5,2%-%date:~8,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%    start install git>>"%rootdirx%/log.txt"
 start /wait %tmpdirx%/git.exe 
 Echo %date:~0,4%-%date:~5,2%-%date:~8,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%    installed git>>"%rootdirx%/log.txt"
+REM cls
 goto gitCheck
 
 :githubAddr
@@ -210,6 +224,7 @@ cd %rootdirx%
 set /p github_addr=Please input github_addr:
 echo github_addr=%github_addr%
 rem find ^"github_addr^" %config_file% >>nul && goto configCheck||echo github_addr not founded && goto githubAddr
+REM cls
 goto erroff
 
 :oschinaAddr
@@ -217,15 +232,18 @@ cd %rootdirx%
 set /p oschina_addr=Please input github_addr:
 echo oschina_addr=%oschina_addr%
 rem find ^"oschina_addr^" %config_file% >>nul && goto configCheck||echo github_addr not founded && goto githubAddr
+REM cls
 goto erroff
 
 :mainrepo
 cd %rootdirx%
 echo main repo err 
+REM cls
 goto configCheck
 
 
 :checkSyncDir
+@title  checkSyncDir...
 cd %rootdirx%
 if exist %rootdirx%%sync_dir% (
 echo SYNC start  %rootdirx%%sync_dir%
@@ -244,14 +262,42 @@ cd  %rootdirx%%sync_dir%
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/ntxt168/ntxt168.com/master/favicon.ico -OutFile %rootdirx%%sync_dir%/favicon.ico " 
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/ntxt168/ntxt168.com/master/CNAME -OutFile %rootdirx%%sync_dir%/CNAME "
 Echo %date:~0,4%-%date:~5,2%-%date:~8,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%    sync-dir-check-ok>>"%rootdirx%/log.txt"
+REM cls
 goto checkSyncDir
 )
 
 
 
 :copyFiles
-echo 60秒后开始
-ping 127.0 -n 16 >nul
+set timeremain=15
+echo %timeremain%秒后开始
+for /L %%a in (
+%timeremain%,-1,0
+) do (
+cls
+@title  剩余%%a秒开始...
+if %%a lss 10  echo 准备同步此时请勿操作文件夹 》》》》》》》》》》》》》》》》》》》》》》
+echo     一切准备就绪了，你有5分钟时间按下列顺序操作，剩余%%a秒开始...
+echo 。    
+echo     1.打开excel文件-点击【文件】-》》》点击【另存为】
+echo 。    
+echo     2.点其他位置或者点击上次保存的位置【定位到bat文件同目录的ntxt168下】
+echo 。    
+echo     3.点击【文件类型】-选择【网页】注意不是单个网页
+echo 。    
+echo     4.文件名填入【index】
+echo 。    
+echo     5.如果上传出现乱码，丢失图片，或者其他异常问题，请按照下面的操作进行操作
+echo 。    
+echo        一、保存的时候点击【工具】
+echo        二、点击【web选项】
+echo        三、点击【编码】选项卡
+echo        四、下面选择将此文档另存为选择【Unicode（UTF8）】即可
+if %%a lss 10  echo 准备同步此时请勿操作文件夹 》》》》》》》》》》》》》》》》》》》》》》
+
+ping -n 2 localhost 1>nul 2>nul
+cls
+)
 if not exist %tmpdirx% (
 goto begin
 )
@@ -266,14 +312,19 @@ Echo %date:~0,4%-%date:~5,2%-%date:~8,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%    
 xcopy  /Y/E/C %rootdirx%%sync_dir%\*.*  %tmpdirx%\oschina\   
 xcopy /Y/E/C  %rootdirx%%sync_dir%\*  %tmpdirx%\oschina\    
 REM xcopy /Y /V /S /T /E /Q /C /H /U  %rootdirx%%sync_dir%\*.* %tmpdirx%\github\ >>"%rootdirx%/log.txt"
-echo 拷贝结束。
+echo 拷贝结束。准备添加并提交
 Echo %date:~0,4%-%date:~5,2%-%date:~8,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%    拷贝结束>>"%rootdirx%/log.txt"
+ping -n 2 localhost 1>nul 2>nul
+REM cls
 goto addall
 ) ||  (echo 没有文件
-echo 60秒后重试，没有发现文件
-goto addall
+echo 没有发现文件,尝试查看TMP文件修改
 Echo %date:~0,4%-%date:~5,2%-%date:~8,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%    sync file empty>>"%rootdirx%/log.txt"
+ping -n 2 localhost 1>nul 2>nul
+REM cls
+goto addall
 )
+
 
 :addall
 if not exist %tmpdirx% (
@@ -284,11 +335,21 @@ if not exist %tmpdirx%\oschina (
 echo  没有发现oschina
 goto begin
 )
-cd %tmpdirx%\oschina\
+echo 准备添加文件
+cd %rootdirx%
+cd %tmpdirx%\oschina
 git add . >>"%rootdirx%/log.txt"
-git status >>"%rootdirx%/log.txt"
-rem git  -C %tmpdirx%\github\ add .
-goto commit
+git status >"%tmpdirx%\oschina_status"
+for /f "delims=" %%a in (%tmpdirx%\oschina_status) do ( 
+	for /f "tokens=1* delims=:" %%i in ('call echo %%a^|find /i "Changes not staged for commit"') do (
+		Echo %date:~0,4%-%date:~5,2%-%date:~8,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%    %%a>>"%rootdirx%/log.txt"
+		set gitstatus=%%a
+		)  
+) 
+echo %gitstatus%|find /i "Changes not staged for commit" >nul && echo Changes not staged for commit  || echo Changes staged for commit
+echo %gitstatus%|find /i "Changes not staged for commit" >nul && git add .   || echo all added nothing to add
+echo %gitstatus%|find /i "Changes not staged for commit" >nul && goto commit || goto push
+
 
 :commit
 if not exist %tmpdirx% (
@@ -297,6 +358,18 @@ goto begin
 if not exist %tmpdirx%\oschina (
 goto begin
 )
+cd %rootdirx%
+cd %tmpdirx%\oschina
+git status >"%tmpdirx%\oschina_status"
+for /f "delims=" %%a in (%tmpdirx%\oschina_status) do ( 
+	for /f "tokens=1* delims=:" %%i in ('call echo %%a^|find /i "Changes to be committed"') do (
+		Echo %date:~0,4%-%date:~5,2%-%date:~8,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%    %%a>>"%rootdirx%/log.txt"
+		set gitstatus=%%a
+		)  
+) 
+echo %gitstatus%|find /i "Changes to be committed" >nul && echo Changes to be committed || echo no Changes to be committed
+echo %gitstatus%|find /i "Changes to be committed" >nul && echo 准备提交 || goto push
+echo 准备提交请求
 set hh=%time:~0,2%
 set mi=%time:~3,2%
 set ss=%time:~6,2% 
@@ -314,9 +387,13 @@ REM echo oschina_addr=%oschina_addr%
 cd %tmpdirx%\oschina\
 git   commit -m [%yy%-%mm%-%dd%_%Thh%.%Tmm%.%Tss%] >>"%rootdirx%/log.txt"
 rem git  -C %tmpdirx%\github\ commit -m [%date:~0,4%-%date:~5,2%-%date:~8,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%]
+echo 请求提交结束，准备上传发布
+ping -n 2 localhost 1>nul 2>nul
+REM cls
 goto push
 
 :push
+
 echo start push
 if not exist %tmpdirx% (
 goto begin
@@ -324,30 +401,46 @@ goto begin
 if not exist %tmpdirx%\oschina (
 goto begin
 )
+echo 准备发布
 cd %rootdirx%
 REM echo github_addr=%github_addr%
 REM echo oschina_addr=%oschina_addr%
 cd %tmpdirx%\oschina\
+git status >"%tmpdirx%\oschina_status"
+for /f "delims=" %%a in (%tmpdirx%\oschina_status) do ( 
+	for /f "tokens=1* delims=:" %%i in ('call echo %%a^|find /i "publish your local commits"') do (
+		Echo %date:~0,4%-%date:~5,2%-%date:~8,2%_%TIME:~0,2%.%TIME:~3,2%.%TIME:~6,2%    %%a>>"%rootdirx%/log.txt"
+		set gitstatus=%%a
+		)  
+) 
+echo %gitstatus%|find /i "publish your local commits" >nul && echo publish your local commits || echo 取消发布
+echo %gitstatus%|find /i "publish your local commits" >nul && echo 准备提交 || goto copyFiles
 git status >>"%rootdirx%/log.txt" 
 git push  origin master >>"%rootdirx%/log.txt"
-
 rem git  -C %tmpdirx%\github\ push origin master
-echo  push sucess
+echo  发布成功
+ping -n 2 localhost 1>nul 2>nul
+REM cls
 goto del
 
 :del
+echo 删除同步文件
 if not exist %tmpdirx% (
 goto begin
 )
 if not exist %tmpdirx%\oschina (
 goto begin
 )
+if not exist %rootdirx%%sync_dir% (
+goto begin
+)
 echo delete all sync file
 ping 127.1 -n 10 >nul
 cd %rootdirx%
-rd  /S /Q %rootdirx%%sync_dir% 
-md %rootdirx%%sync_dir% 
-cd  %rootdirx%%sync_dir% 
+del  /S /Q %rootdirx%%sync_dir%\*.*
+del  /S /Q %rootdirx%%sync_dir%\* 
+echo 文件夹已清空
+ping -n 2 localhost 1>nul 2>nul
 goto checkSyncDir
 
 
